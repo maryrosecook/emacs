693c11a41f036e3c226d1503ac1fbdb381d3a15d
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-11-24 13:34:32 +0000

    Use ruby mode for rake files etc.

--- init.el
+++ init.el
@@ -67,6 +67,15 @@
     (peepopen-goto-file-gui)))
 (global-set-key [(meta ?o)] 'open)
 
+;; set ruby highlighting for files without .rb extension
+(add-to-list 'auto-mode-alist '("\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+
 ;; inserts js log call and puts cursor between brackets
 (defun js-insert-console ()
   (interactive)
