0bac972644daec3a63dd577d4ba6558ef8c7b959
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-12-21 15:13:33 +0000

    Use JS2 mode for JavaScript.

--- init.el
+++ init.el
@@ -14,6 +14,10 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
 (require 'coffee-mode)
 
+;; js2 mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 ;; vc-diff colours
 (require 'diff-mode-)
 
