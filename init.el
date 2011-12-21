d807ce77e09f18965e24062c34cdf68ad37ab006
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-12-21 14:57:16 +0000

    add other markdown file extensions to list

--- init.el
+++ init.el
@@ -6,7 +6,9 @@
 (add-hook 'ruby-mode-hook (lambda () (local-set-key "\r" 'newline-and-indent)))
 
 ;; markdown
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
 ;; coffeescript major mode
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
