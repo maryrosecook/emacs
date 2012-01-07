983faf870eabe3914b8e148481b71709bf41559b
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-01-07 16:56:55 +0000

    No more JS2 mode.  Formatting too weird.

--- init.el
+++ init.el
@@ -14,10 +14,6 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
 (require 'coffee-mode)
 
-;; js2 mode
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
 ;; vc-diff colours
 (require 'diff-mode-)
 
