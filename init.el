2d4d4456b83e778102c72230ee08400fc6a7ec8c
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-12-27 18:00:58 +0000

    newest

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 ;; load paths
@@ -19,18 +21,36 @@
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
+;; enable auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-auto-show-menu nil)
+
+;; tern (JS tooling)
+(add-to-list 'load-path "~/.emacs.d/vendor/tern/emacs/")
+(autoload 'tern-mode "tern.el" nil t)
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
+
 ;; ioke mode
 ;; (require 'ioke-mode)
+
+;; markdown mode with support for other langs
+(setq load-path
+      (append '("~/.emacs.d/polymode/" "~/.emacs.d/polymode/modes")
+              load-path))
+
+(require 'poly-R)
+(require 'poly-markdown)
 
 (require 'dash)
 
 ;; Make C-u equivalent to C-x (for my dvorak keyboard)
 (keyboard-translate ?\C-x ?\C-u)
 (keyboard-translate ?\C-u ?\C-x)
-
-;; enable auto-complete
-;; (require 'auto-complete-config)
-;; (ac-config-default)
 
 ;; peg mode
 (autoload 'peg-mode "peg-mode" "Mode for editing PEG grammar files" t)
@@ -57,7 +77,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" "b428cf84ac4401de1e496e5450514589f7877ca3cfa6a9db48b4666c93e4c466" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "3527fd78ef69e7f42481e6de5bf7782b5552a88bfe2a600cf9734b7a6d89b33f" "d903c8447aef4cd3a4c66b965a18fdc25b718c64f764e26a09547d56702966e7" "4d6f242944f3f2707c3119ad3ec64faf5f6f56e0916823d53db362655b767bbe" "152e9e6fe1e1caa8f3927c97500c1aa673faa60518f2c14f9df72cf05e81819e" "dbd1eca1883c12b233661eaccef2e6a6c4d1e356f2be7265e6964e10bd58131f" "30f4892a1476eba37e667ee4d61eff7e0db6a6b74254b2551b36b976693f5f17" "c739ff147328c4a290e2d66c88e66e7e408c59249b53b3cedba77984a3d992ef" "4bb8b36b52047435938386dadf6b464122237510c529f088b54ea18cddfd93fe" "a17154cda66454ed83a839ad826defb9fdf8e984e66a4e1f908cc494e8b007c2" "f46c9db60a95e80650fa0dd094361ecb0b4669ae311b949fd0dcb5bc7ba44763" "f111203851bd38e84e3163958ee26845cd8cad470188a565c1b2b4ed41d0403b" "d64645be2fe83276a7bdaaff9d4b04188b9a7dd3a7e14e77089febfe0ec7d978" "544d7434608e7b1754727279113d2343281476622d52a0178de6f2ff237b8814" "f21ef039662c30235077b4bec1aaa3fc306ad34398705c705d408cf2f9feeaae" "c61a5b5169f7048a35c0920f5ca30ab9e8b11fc8ac28b59393eb8bbb27b21f93" "b282d9643248c2d67aa44fafb40d33bb96df043ca09c2bf60b8da293b3ec6b6b" "bb877d4214b47342c90017edb7c2e617d08b9a662336098db6f19831cdb4c612" "ccdae8fc59cf9513f01686b97a984af25d52463fcb3ac00d89e9150f5ea19202" "1abc774ca2a17d489311b5efb1f0e2ea3ec3ce540c6bcae6c88a45166199e5ec" "002527bc073d6002886475b3d06d7e866dd1f7a3e81e6e343e807f9d76c4319c" "75ac2cded84e96b333c6e72b17c4d508f0924c4b7aeb576add4c533f7cb6629f" "70d72e74992e2d7ef7df523d9453b002e3cb7bb5122cdcee71559d87ba0060dc" "405d2f0c9a2e820c20194a9fe956f94338a705884dcf2da2f67269bd271b0bb6" "f3725235eab91da0735c511903e3eb78888f2205e3d2c11af7f821dd2eeb6067" "2d3e8051bb8511206783f26bc3294e6bbbe84091789359d8ae3cf03c81683d40" "5144a17c5cce9a5fd35359eb484ca4e24fcccb870a81cef46b0a4fcb70ab44d9" "0c7cde9f11ff3defa575c58b2098f6d2bc7d11b66bdda0fc3ebe834665abb0d4" "8f59315a266c1a2bbdee05c6e74233defdb1481bbe0081f6fab09e598b999811" "50441a814c8e77b847da002303af8d235798ca9432ccd2e3a5ed02baa7d2985a" "0607b7b69b20184b1d26deed4516770c8438733a8bab0efce7ed8cef1e5125e3" "efe8dcbd9bfa6e08c1251a0d5b5c6e4286623a842e7c37c0b80f2fde9892c3c2" "59b24a289a3de847002013d953affd98478f9568b89147cf088637ac1daa1220" "53d38354806bf131aaa480dfb6fc9d11c0dc9446b2849ce5372192e7c7d4960f" "5dc055485914ba21a45667ea9431c51d9c117bebf81f33090a0a1d8a534c09b7" "c1810f6d0951a3b527b56e5e26795fd86d89af0af98d16e3044b014877d064d4" "cc0edbbda20716c1e2ab5a6e2f9a750cd09ba04a15fc4cc87f657a28d405916e" "8fe6e31ad7c2033f97ec624b5dab8509bca66e430fe769cf1e181f47808afce7" "a3ef63aefbecb00c5987b483137cc43e2312497348715a9dff44144328e475fc" "9c3911a8beff0c60a967905b9f5ae178b8f62bf8ebce5be5619b5cc21221937d" "1e50e1ff2d7779891a6bac2aa5446834bf44ef5368f8df25ee26b42af6888760" "81efe6e406c886e71c278720927ae9695617643f42ee704e93a771966904409e" "7c9886b27a05a1ea30c8848cc287e37f118efb5613e9a9b0703300b2e69f99d6" "942572f137fc50f58cb73bceaf1a81f80d865e7b054a5dc71d538da6b295692a" "ecb261a7afeb1cadddf0071310ebd59eacc651b4128f22bb90970ed00cbb0079" "5b13f5883ecda4a00c5f48bfa47c5bce5a4eebda848c08c332ad9c1adac6064c" "9529db2ee385266ab6a359e25629931f589d4630e296466cdf2207b0ede8809b" "16a9b6dcf2381379419e15d36c61926e4f3d8925754daa66e9e83617e8ccafdf" "7cec21763c00b258'6c5877e23ea7347f853672c57f81d61806ac8f1dc2379986" "385a4dd1c1369b5301e9c8f38e1984e9e16e0fffaf19b12a8ee9b2431c2fbab0" "fe8f0fe9e52c21a3d574d25ab1167f3372ab37e1e97478fe065c4f8015867abc" "0449a71c940c57f767774e30d7bf28b64456f431510d8cde29e86657a2602df6" "7f5f2d4376315b12a62d3edb921c62dec7884be43d268fefeac52dad7cbf9618" "4294fa1b78ee65d076a1302f6ed34d42e34f637aae918b7691835adef69bd4cc" default))))
+    ("3527fd78ef69e7f42481e6de5bf7782b5552a88bfe2a600cf9734b7a6d89b33f" default))))
 
 ;; add package manager
 (add-to-list 'load-path "~/.emacs.d/src/packages/")
@@ -217,7 +237,7 @@
   (interactive)
   (let* ((text-dir "~/text/") (code-dir "~/code/scratch/")
          (dirs (list text-dir code-dir))
-         (name (get-next-scratch-file-name dirs 200))
+         (name (get-next-scratch-file-name dirs 350))
          (name (read-string "File name: " name)))
     (if (string-match "[.]+" name)
         (new-buffer-with-ready-file code-dir name)
@@ -237,6 +257,7 @@
 ;; widescreen, no line-wrap
 (defun write-code ()
   (interactive)
+  (show-paren-mode 1)
   (disable-theme 'adwaita)
   (load-theme 'blackboard)
   (set-cursor-color "#ffffff")
@@ -246,7 +267,6 @@
   (set-frame-height (selected-frame) 55)
   (visual-line-mode 0)
   (setq line-spacing 0)
-  (show-paren-mode 1)
 
   (setq mode-line-format
     (list "  "
@@ -255,8 +275,6 @@
           "%m"
           ;; 'minor-mode-alist ;; don't show minor modes
           '(global-mode-string (global-mode-string)))))
-
-
 
 ;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
 (defun rename-file-and-buffer (new-name)
@@ -343,11 +361,9 @@
 (global-set-key (read-kbd-macro "C-x j") 'insert-code-tags)
 (global-set-key (read-kbd-macro "C-x t") 'toggle-frame-fullscreen)
 
-(global-set-key (read-kbd-macro "M-s") 'query-replace)
+(global-set-key (read-kbd-macro "M-s") 'query-replace-regexp)
 (global-set-key "\C-x\C-r" 'revert-buffer-no-confirm) ;; remap revert buffer
 (global-set-key "\C-x\M-r" 'rename-file-and-buffer)
-
-;; (modify-syntax-entry ?_ "w" js-mode-syntax-table)
 
 ;; map start of file and end of file commands to nicer key combos
 (global-set-key (read-kbd-macro "M-[") 'beginning-of-buffer)
@@ -443,5 +459,9 @@
 
 ;; don't use OS X's native fullscreen
 (setq ns-use-native-fullscreen nil)
+
+;; do automatic wrapping of comments
+(require 'newcomment)
+(setq comment-auto-fill-only-comments 1)
 
 (write-code)
